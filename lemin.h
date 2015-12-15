c9dc2b93b02b52826393d621a5fffc49ba5bdcd1
Nakiboudine MOHAMED <nmohamed@e2r8p14.42.fr>
2015-12-15 17:25:03 +0100

    pseudocode to code

--- lemin.h
+++ lemin.h
@@ -6,37 +6,40 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 17:46:33 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/12/08 18:12:35 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/12/15 17:24:46 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
-# define T_RL t_room_list
 # define INFINITE 0x7fffffff
-# define NO_SPECIAL 0
-# define IS_START 1
-# define IS_END 2
 
-typedef struct s_room		t_room;
-typedef struct s_room_list	t_room_list;
+typedef struct s_room t_room;
+typedef struct s_neighbour t_neighbour;
 
-struct	s_room_list
+struct s_room
 {
-	t_room		*room;
-	t_room_list	*next;
+	char		*name;
+	t_room		*next;
+	t_neighbour	*next_neighbour;
 };
 
-struct	s_room
+struct s_neighbour
 {
-	int		x;
-	int		y;
-	char	*name;
-	int		distance;
-	T_RL	*other_rooms;
+	room		*room;
+	t_neighbour	*next;
 };
 
-t_room_list		*room_push_back(t_room_list *head, t_room *new_room);
-t_room			*room_find_by_name(t_room_list *head, char *name);
+extern t_room	*g_room_list;
+extern char		*g_next_attr;
 
-#endif
+void			parse_room(char *line);
+void			room_push_front(t_room **room_head, t_room *room_to_copy);
+void			parse_tube(char *line);
+void			neighbour_push_front(t_neighbour **neighbour, t_room *room);
+void			room_find_by_name(t_room *room, char *name);
+void			calculate_route(t_room *room, distance);
+void			follow_route(t_room *room);
+void			get_closest_neighbour(t_room *room);
+
+endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 17:26:41 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/12/15 15:27:45 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/12/15 17:24:29 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,83 @@ char	*ft_get_line(int const fd)
 	if ((nl = ft_strchr(line, '\n')) != NULL)
 		*nl = '\n';
 	return (line);
+}
+
+void	parse_room(char *line);
+
+void	room_push_front(t_room **room_head, t_room *room_to_copy);
+
+void	parse_tube(char *line)
+{
+	char	**token;
+
+	token = ft_strsplit(line, '-');
+}
+
+void	neighbour_push_front(t_neighbour **neighbour, t_room *room)
+{
+	t_neighbour		*new_neighbour;
+
+	new_neighbour = malloc(sizeof(*new_neighbour));
+	new_neighbour->room = room;
+	new_neighbour->new_neighbour = *neighbour;
+	*neighbour = new_neighbour;
+}
+
+void	room_find_by_name(t_room *room, char *name)
+{
+	while (room != NULL)
+	{
+		if (ft_strequ(name, room->name))
+		{
+			return (room);
+		}
+	}
+	return (NULL);
+}
+
+void	calculate_route(t_room *room, distance)
+{
+	t_neighbour	*neighbour;
+
+	distance = distance + 1;
+	if (room != NULL)
+	{
+		neighbour = room.next_neighbour;
+		while (neighbour != NULL)
+		{
+			neighbour->room->distance = distance;
+			calculate_route(neighbour.room);
+			neighbour = neighbour->next;
+		}
+	}
+}
+
+void	follow_route(t_room *room)
+{
+	while (!ft_strequ(room->attr, "##end"))
+	{
+		room = get_closest_neighbour(room);
+		putstr("PLACEHOLDER");
+	}
+}
+
+void	get_closest_neighbour(t_room *room)
+{
+	t_neighbour	*n;
+	t_neighbour	*closest;
+
+	n = room->next_neighbour;
+	closest = n;
+	while (n != NULL)
+	{
+		if (n->room->distance < closest->room->distance)
+		{
+			closest = n->room;
+		}
+		n = n->next_neighbour;
+	}
+	return (closest);
 }
 
 int		main(void)
